fb09cf8ce7ffd94a1c1da8dcaba25756041b6b22
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-05 21:20:06 +0200

    add postgre db

--- validationForm/auth.php
+++ validationForm/auth.php
@@ -4,7 +4,7 @@
 
     $pass = md5($pass."gmbh");
 
-    $dbconn = pg_connect("host=localhost dbname=postgres user=postgres password=root")
+    $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
     or die('Не удалось соединиться: ' . pg_last_error());
     $query = "SELECT * FROM \"public\".\"Users\" WHERE \"login\" = '$login' AND \"password\" = '$pass'";
     $result = pg_query($dbconn,$query);
--- validationForm/check.php
+++ validationForm/check.php
@@ -16,7 +16,7 @@
     }
     $pass = md5($pass."gmbh"); // хеш пароля
 
-    $dbconn = pg_connect("host=localhost dbname=postgres user=postgres password=root")
+    $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
         or die('Не удалось соединиться: ' . pg_last_error());
     $query = "INSERT INTO \"public\".\"Users\"(\"login\",\"name\",\"password\") VALUES ('$login','$name','$pass')";
     $result = pg_query($dbconn,$query) or die('Ошибка запроса: ' . pg_last_error());
