a70393d4de20d492532bd3caa41d9d089284341b
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 10:28:06 +0200

    Update check.php

--- validationForm/check.php
+++ validationForm/check.php
@@ -14,9 +14,10 @@
         echo "Недопустимая длина пароля(от 4 до 15 символов)";
         exit();
     }
-    $pass = md5($pass."gmbh"); // хеш пароля
 echo $pass;
 die();
+    $pass = md5($pass."gmbh"); // хеш пароля
+
     $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
         or die('Не удалось соединиться: ' . pg_last_error());
     $query = "INSERT INTO public.Users(\"login\",\"name\",\"password\") VALUES ('$login','$name','$pass')";
