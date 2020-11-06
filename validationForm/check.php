ae07b2482f5e504075b538c87bfaceace714f856
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 10:36:09 +0200

    Update check.php

--- validationForm/check.php
+++ validationForm/check.php
@@ -1,11 +1,9 @@
 <?php
 error_reporting(E_ALL);
 
-echo 0;
     $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
     $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
     $pass = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
-echo 1;
 
     if(strlen($login) < 5 || strlen($login) > 15){
         echo "Недопустимая длина логина";
@@ -18,7 +16,6 @@ echo 1;
         echo "Недопустимая длина пароля(от 4 до 15 символов)";
         die();
     }
-echo 2;
     $pass = md5($pass."gmbh"); // хеш пароля
 
     $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
