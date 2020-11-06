52ce78b22a4afffc99757db5630476b463d403dc
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 10:43:01 +0200

    Update check.php

--- validationForm/check.php
+++ validationForm/check.php
@@ -20,7 +20,7 @@ error_reporting(E_ALL);
 
     $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
         or die('Не удалось соединиться: ' . pg_last_error());
-    $query = "INSERT INTO public.Users(\"login\",\"name\",\"password\") VALUES ('$login','$name','$pass')";
+    $query = "INSERT INTO "Users"(\"login\",\"name\",\"password\") VALUES ('$login','$name','$pass')";
     $result = pg_query($dbconn,$query) or die('Ошибка запроса: ' . pg_last_error());
 
     header('Location: /index.php');
