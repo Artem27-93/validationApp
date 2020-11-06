ab28ddb429caf2c56057faf433698f1f5540e43a
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 10:51:56 +0200

    Update auth.php

--- validationForm/auth.php
+++ validationForm/auth.php
@@ -6,7 +6,7 @@
 
     $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
     or die('Не удалось соединиться: ' . pg_last_error());
-    $query = "SELECT * FROM \"public\".\"Users\" WHERE \"login\" = '$login' AND \"password\" = '$pass'";
+    $query = "SELECT * FROM \"Users\" WHERE \"login\" = '$login' AND \"password\" = '$pass'";
     $result = pg_query($dbconn,$query);
     $user = pg_fetch_assoc($result);
 
