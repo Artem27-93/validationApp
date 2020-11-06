6ad24a1f11310d01358ebb7c4013ddcba25500a9
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 13:07:47 +0200

    Update getTicket.php

--- getTicket.php
+++ getTicket.php
@@ -1,7 +1,7 @@
 <?php
 $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
 or die('Не удалось соединиться: ' . pg_last_error());
-$query = "select C.\"Category\", t1.\"Path2File\",\"Question\",\"CorrectVariant\"
+$query = "select id,C.\"Category\", t1.\"Path2File\",\"Question\",\"CorrectVariant\"
 from (select * from \"Queue\" where \"isValid?\" is null
       order by \"CreateDT\" desc limit 1) t1
     left join \"Category\" C on t1.\"Category\" = C.\"Category\";";
