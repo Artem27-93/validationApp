7c42bbc70feb6fa3b9e712f64fc14a0d97beb3fd
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 10:26:35 +0200

    Update check.php

--- validationForm/check.php
+++ validationForm/check.php
@@ -1,8 +1,5 @@
 <?php
     $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
-
-echo $login;
-die();
     $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
     $pass = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
 
@@ -18,10 +15,11 @@ die();
         exit();
     }
     $pass = md5($pass."gmbh"); // хеш пароля
-
+echo $pass;
+die();
     $dbconn = pg_connect("host=ec2-54-76-215-139.eu-west-1.compute.amazonaws.com dbname=dcbpsp0k5sldbg user=rnqvwmovcyqzod password=46cef4c9e7ac99a7752b9dfad18047e3c49cc77e4edaf279748d5456ca09b4a7")
         or die('Не удалось соединиться: ' . pg_last_error());
-    $query = "INSERT INTO \"public\".\"Users\"(\"login\",\"name\",\"password\") VALUES ('$login','$name','$pass')";
+    $query = "INSERT INTO public.Users(\"login\",\"name\",\"password\") VALUES ('$login','$name','$pass')";
     $result = pg_query($dbconn,$query) or die('Ошибка запроса: ' . pg_last_error());
 
     header('Location: /index.php');
