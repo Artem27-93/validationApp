88a533f09320f8f4e694e67f8e8b93477aea81c4
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-11-06 10:15:17 +0200

    Update check.php
    test

--- validationForm/check.php
+++ validationForm/check.php
@@ -1,5 +1,8 @@
 <?php
     $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
+
+echo $login;
+die();
     $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
     $pass = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
 
